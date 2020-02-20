b05685fc3c92e899953b643f52ec98e1f68dfe28
Shuja Rizvi <shujj.rizvi@hotmail.com>
2020-02-20 17:39:57 +0500

    Initial commit

---
+++ EventArguments/ListItemAddEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItemListView.EventArguments
+{
+    public class ListItemAddEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Index in the list where the item was inserted.
+        /// </summary>
+        public int Index { get; set; }
+    }
+}
---
+++ ItemListView.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Collections;
+using System;
+using ItemListView.EventArguments;
+using System.Diagnostics;
+
+namespace ItemListView
+{
+    public partial class ItemListView : UserControl
+    {
+        private IList itemList;
+        private event EventHandler<ListItemAddEventArgs> OnAdd;
+        private event EventHandler<ListItemAddEventArgs> OnRemove;
+
+        public ItemListView()
+        {
+            InitializeComponent();
+            this.OnAdd += ItemListView_OnAdd;
+            this.OnRemove += ItemListView_OnRemove;
+        }
+
+
+        /// <summary>
+        /// Iniitlizes the list with the provided type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        public void Initialize<T>()
+        {
+            itemList = new List<T>();
+        }
+
+        /// <summary>
+        /// Inserts an Item into the list.
+        /// </summary>
+        public void Add<T>(T item)
+        {
+            var insertionIndex = itemList.Add(item);
+            OnAdd(this, new ListItemAddEventArgs
+            {
+                Index = insertionIndex
+            });
+        }
+
+
+        /// <summary>
+        /// Removes the given item from the list.
+        /// </summary>
+        /// <typeparam name="T">Generic type given for the list.</typeparam>
+        /// <param name="item">Item to be removed from list.</param>
+        /// <returns>Returns true if removal is successful, false if item is not found.</returns>
+        public bool Remove<T>(T item)
+        {
+            itemList.Remove(item);
+            return true;
+        }
+
+        /// <summary>
+        /// Removes an item at the given index.
+        /// </summary>
+        /// <param name="index">The list index to remove item at.</param>
+        public void RemoveAt(int index)
+        {
+            itemList.RemoveAt(index);
+        }
+
+        #region EVENT HANDLERS
+        private void ItemListView_OnAdd(object sender, ListItemAddEventArgs e)
+        {
+            Debug.WriteLine(e.Index);
+        }
+
+        private void ItemListView_OnRemove(object sender, ListItemAddEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
