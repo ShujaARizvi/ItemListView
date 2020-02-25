7c8dd28beaa88fda60634812abb8252b189015e1
Shuja Rizvi <shujj.rizvi@hotmail.com>
2020-02-25 15:48:52 +0500

    Changes in OnItemClickListener.

--- Adapters/ListAdapter.cs
+++ Adapters/ListAdapter.cs
@@ -2,6 +2,7 @@
 using ItemListView.EventArgs;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace ItemListView.Adapters
@@ -15,10 +16,23 @@ namespace ItemListView.Adapters
         private int position = 5;
         private readonly int factor = 2;
 
+        public int SelectedIndex { get; private set; }
+
+        /// <summary>
+        /// UI Elements that are displayed on the list.
+        /// Access to them give developers ability to modify the controls at their will.
+        /// </summary>
+        public System.Windows.Forms.Control.ControlCollection Controls {
+            get 
+            {
+                return itemsPanel.Controls;
+            }
+        }
         private event EventHandler<OnItemClickEventArgs<Control>> OnItemClick;
 
         public ListAdapter(List<Item> items)
         {
+            SelectedIndex = -1;
             dataItems = items;
             uiListElements = new List<Control>();
             OnItemClick += OnItemClickListener;
@@ -54,13 +68,17 @@ namespace ItemListView.Adapters
                 Control curElement = this.Draw(new Control(), item);
                 curElement.Click += (s, e) =>
                 {
-                    OnItemClick(s, new OnItemClickEventArgs<Control> { Index = index++, Item = curElement });
+                    OnItemClick(s, new OnItemClickEventArgs<Control> { Index = index, Item = curElement });
                 };
+                curElement.Name = index.ToString(); index++;
                 uiListElements.Add(curElement);
             }
         }
         
-        public virtual void OnItemClickListener(object sender, OnItemClickEventArgs<Control> e) {}
+        public virtual void OnItemClickListener(object sender, OnItemClickEventArgs<Control> e) 
+        {
+            this.SelectedIndex = e.Index;
+        }
 
         public abstract Control Draw(Control control, Item item);
     }
